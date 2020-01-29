b0a120f30eef8c3abca9516d7dc25c6928f92918
duro06 <andi.meka.025@gmail.com>
2020-01-29 23:22:18 +0700

    modifikasi main.js, tambah data cats.js dan dogs.js, modifikasi cats.vue untuk ambil data dari cats.js berlaku juga untuk dogs.vue

---
+++ src/data/cats.js
@@ -0,0 +1,51 @@
+export default [{
+    name: 'heli',
+    breed: 'persia',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weight: 34,
+    location: 'fourside',
+    notes: 'pure breed. Trained for competition'
+  },
+  {
+    name: 'Hillary',
+    breed: 'mut',
+    gender: 'female',
+    age: 17,
+    color: 'orange/white',
+    weight: 37,
+    location: 'threed',
+    notes: 'super friendly.'
+  },
+  {
+    name: 'heli 2',
+    breed: 'anggora 2',
+    gender: 'male',
+    age: 7,
+    color: 'black/green',
+    weight: 34,
+    location: 'fourside2',
+    notes: '2pure breed. Trained for competition'
+  },
+  {
+    name: 'heli 3',
+    breed: 'collie 3',
+    gender: 'female',
+    age: 7,
+    color: 'black/white 3',
+    weight: 34,
+    location: 'fourside 3',
+    notes: 'pure breed3. Trained for competition'
+  },
+  {
+    name: 'heli 4',
+    breed: 'collie 4',
+    gender: 'female',
+    age: 7,
+    color: 'black/white 4',
+    weight: 34,
+    location: 'fourside 4',
+    notes: 'pure breed 4. Trained for competition'
+  }
+]
---
+++ src/data/dogs.js
@@ -0,0 +1,51 @@
+export default [{
+    name: 'sheeba',
+    breed: 'collie',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weight: 34,
+    location: 'fourside',
+    notes: 'pure breed. Trained for competition'
+  },
+  {
+    name: 'Hillary',
+    breed: 'mut',
+    gender: 'female',
+    age: 17,
+    color: 'orange/white',
+    weight: 37,
+    location: 'threed',
+    notes: 'super friendly.'
+  },
+  {
+    name: 'sheeba 2',
+    breed: 'collie 2',
+    gender: 'female',
+    age: 7,
+    color: 'black/green',
+    weight: 34,
+    location: 'fourside 2',
+    notes: '2 pure breed. Trained for competition'
+  },
+  {
+    name: 'sheeba 3',
+    breed: 'collie 3',
+    gender: 'female',
+    age: 7,
+    color: 'black/white 3',
+    weight: 34,
+    location: 'fourside 3',
+    notes: 'pure breed 3. Trained for competition'
+  },
+  {
+    name: 'sheeba 4',
+    breed: 'collie 4',
+    gender: 'female',
+    age: 7,
+    color: 'black/white 4',
+    weight: 34,
+    location: 'fourside 4',
+    notes: 'pure breed4. Trained for competition'
+  }
+]
